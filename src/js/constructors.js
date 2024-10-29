299013224d6924e3779974b30fe3d09b9e576b47
jonorl <39122230+jonorl@users.noreply.github.com>
2024-10-29 15:51:23 +0300

    JS modules created and working fine Next step is to populate the data back in the DOM

---
+++ src/js/constructors.js
@@ -0,0 +1,10 @@
+export class todo {
+    constructor(id, title, description, dueDate, priority, notes) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
+};
