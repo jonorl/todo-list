b836dc61614b05fd3ff46899eccd288834c5afbf
jonorl <39122230+jonorl@users.noreply.github.com>
2024-11-04 16:51:43 +0300

    Linting and formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+export default {
+  ...pluginJs.configs.recommended,
+  languageOptions: { globals: globals.browser },
+  rules: {
+    "no-unused-vars": 1,
+    "no-undef": 1
+  },
+};
